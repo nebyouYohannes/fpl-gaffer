12178f40c10ddb33da206921e0d7f4fbc6755d0a
nebyou <nebyouyohannes1@gmail.com>
2026-04-20 16:58:05 +0300

    removed all uppercase

--- src/Pages/ComingSoonPage.tsx
+++ src/Pages/ComingSoonPage.tsx
@@ -1,11 +1,11 @@
 export default function ComingSoonPage() {
   return (
     <div className="min-h-screen w-full bg-fpl-purple flex flex-col items-center justify-center text-center gap-4">
-      <h1 className="text-white text-3xl md:text-5xl font-bold tracking-tighter uppercase">
+      <h1 className="text-white text-3xl md:text-5xl font-bold tracking-tighter">
         fpl<span className="text-fpl-green">Gaffer</span>
       </h1>
 
-      <h2 className="text-fpl-green/80 text-l md:text-xl font-light tracking-widest uppercase animate-pulse">
+      <h2 className="text-fpl-green/80 text-l md:text-xl font-light tracking-widest  animate-pulse">
         Coming Soon...
       </h2>
     </div>
