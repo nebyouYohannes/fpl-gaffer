329d5757d87236a6919ba541042225383e7c90ec
nebyou <nebyouyohannes1@gmail.com>
2026-04-20 16:55:43 +0300

    lowercase

--- src/Pages/ComingSoonPage.tsx
+++ src/Pages/ComingSoonPage.tsx
@@ -2,7 +2,7 @@ export default function ComingSoonPage() {
   return (
     <div className="min-h-screen w-full bg-fpl-purple flex flex-col items-center justify-center text-center gap-4">
       <h1 className="text-white text-3xl md:text-5xl font-bold tracking-tighter uppercase">
-        FPL<span className="text-fpl-green">Gaffer</span>
+        fpl<span className="text-fpl-green">Gaffer</span>
       </h1>
 
       <h2 className="text-fpl-green/80 text-l md:text-xl font-light tracking-widest uppercase animate-pulse">
